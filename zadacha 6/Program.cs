a41ebaa81e20e37dadf7962be9e26c37b8d5b9a7
Pavel <nntspector@icloud.com>
2022-12-16 22:38:52 +0300

    zadacha 6 reshena

---
+++ zadacha 6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите число: ");
+string userInput1 = Console.ReadLine ()?? "";
+int number1 = int.Parse (userInput1);
+int number2 = number1 % 2;
+if (number2 == 0)
+{
+Console.Write ($"{number1} -> четное");
+}
+else
+{
+Console.Write ($"{number1} -> нечетное");
+}
