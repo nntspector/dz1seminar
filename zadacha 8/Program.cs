137d194ee6c8206807e01136686b9093bde9960d
Pavel <nntspector@icloud.com>
2022-12-17 20:41:16 +0300

    zadacha 8 reshena

---
+++ zadacha 8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число: ");
+int a = int.Parse(Console.ReadLine() ?? "");
+Console.Write($"{a} -> ");
+for (int count = 0; count < a; count++)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count}, ");
+    }
+}
+Console.WriteLine();
