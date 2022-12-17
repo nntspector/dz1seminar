564c1362406dc7ff157872baa56cfe380d43d3de
Pavel <nntspector@icloud.com>
2022-12-17 23:24:40 +0300

    определение нечетных чисел

---
+++ test/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите новое число: ");
+int x = int.Parse (Console.ReadLine () ?? "");
+Console.Write ($"{x} ->");
+for (int count = 0; count < x; count++)
+{
+if (count %2 !=0)
+{
+    Console.Write ($"{count}, ");
+}
+}
+Console.WriteLine ();
