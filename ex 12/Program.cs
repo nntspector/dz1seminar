3cefcfc6e64848d0fe3d4a943c88938b15d8f601
Pavel <nntspector@icloud.com>
2022-12-19 21:31:00 +0300

    ex 12

---
+++ ex 12/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write ("Введите первое число: ");
+string userInput1 = Console.ReadLine ()?? "";
+int a1 = int.Parse (userInput1);
+
+Console.Write ("Введите второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int a2 = int.Parse (userInput2);
+
+if (a1 % a2 == 0)
+{
+    Console.Write ($"{a1}, {a2} -> кратно");
+
+}
+else 
+{
+    int result = a1 % a2;
+    Console.Write ($"{a1}, {a2} -> не кратно, остаток {result}");
+}
+
