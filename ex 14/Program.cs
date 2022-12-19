027a346e701e4b5ac2a35dc76301a90e2352d2f9
Pavel <nntspector@icloud.com>
2022-12-19 21:36:52 +0300

    ex 14

---
+++ ex 14/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write ("Введите первое число: ");
+string userInput1 = Console.ReadLine ()?? "";
+int a1 = int.Parse (userInput1);
+
+if (a1 % 7 ==0 && a1 % 23 == 0)
+{
+
+Console.Write ($"{a1} -> да");
+}
+else 
+{
+    Console.Write ($"{a1} -> нет");
+}
+
+
