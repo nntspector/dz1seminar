0b433bcb6b7f4313de99ccb749178df2c7624bc4
Pavel <nntspector@icloud.com>
2022-12-20 22:34:39 +0300

    zadacha 15 reshena

---
+++ dz 15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите номер дня недели: ");
+string userInput1 = Console.ReadLine() ?? "";
+int a1 = int.Parse(userInput1);
+
+if (a1 == 6)
+{
+    Console.Write ($"{a1} -> выходной день ");
+}
+else if  (a1 == 7)
+{
+    Console.Write ($"{a1} -> выходной день ");
+}
+else if (a1 > 0 && a1 < 6)
+{
+    Console.Write ($"{a1} -> будний день");
+}
