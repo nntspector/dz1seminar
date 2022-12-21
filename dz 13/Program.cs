df7ed293ec0a10fceaf230370b7f262e7ded8d5a
Pavel <nntspector@icloud.com>
2022-12-21 22:45:13 +0300

    zadacha 13 reshena

---
+++ dz 13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int a1 = int.Parse(userInput1);
+if (a1 < 100 && a1 > -100)
+{
+    Console.WriteLine($"{a1} -> третьей цифры нет");
+
+}
+else
+{
+    while (a1 >= 1000)
+    {
+        a1 /= 10;
+    }
+    int a2 = a1 % 10;
+    Console.WriteLine($"{a2} -> третья цифра");
+}
+
