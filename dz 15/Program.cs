a0cd493886ba729e612852d3d22bf31fed98c73c
Pavel <nntspector@icloud.com>
2022-12-22 11:42:59 +0300

    задача 15 оптимизирована

--- dz 15/Program.cs
+++ dz 15/Program.cs
@@ -2,11 +2,7 @@
 string userInput1 = Console.ReadLine() ?? "";
 int a1 = int.Parse(userInput1);
 
-if (a1 == 6)
-{
-    Console.Write ($"{a1} -> выходной день ");
-}
-else if  (a1 == 7)
+if (a1 == 6 || a1 == 7)
 {
     Console.Write ($"{a1} -> выходной день ");
 }
