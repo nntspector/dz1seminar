51a41ceefa84ef16e5d6e562338e5bf8783b7014
Pavel <nntspector@icloud.com>
2022-12-22 21:11:01 +0300

    zadacha 18 reshena

---
+++ ex 18/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите номер четверти: ");
+int a = int.Parse(Console.ReadLine() ?? "");
+
+if(a == 1)
+{
+     Console.WriteLine("X > 0 && Y > 0");
+}
+else if(a == 2)
+{
+    Console.WriteLine("X < 0 && Y > 0");
+}
+else if(a == 3)
+{
+    Console.WriteLine("X < 0 && Y < 0");
+}
+else if(a == 4)
+{
+    Console.WriteLine("X > 0 && Y < 0");
+}
+else
+{
+     Console.WriteLine("Ошибка! четверти не существует");
+}
+
