51cce268fdff35cd69b4117c54e4b649cf9116d8
Pavel <nntspector@icloud.com>
2022-12-24 22:55:59 +0300

    zadacha19 reshena

---
+++ dz 19/Program.cs
@@ -0,0 +1,15 @@
+﻿// var rnd = new Random();
+// int num = rnd.Next(10000, 100000);
+// int a = num/10000;
+// int b = (num/1000) % 10;
+// int c = num % 10;
+// int d = (num % 100)/10;
+
+//  if (a+b == c+d)
+//  {
+//      Console.Write ($"{num} -> является палиндромом");
+//  }
+//  else 
+//  {
+//     Console.Write ($"{num} - >> не является палиндромом");
+// }
