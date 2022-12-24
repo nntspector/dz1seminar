7089f887863b90b792f3a9814a5e2fc35cbdda61
Pavel <nntspector@icloud.com>
2022-12-24 23:28:02 +0300

    dz 23 reshen

---
+++ dz 23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write ("Введите число: ");
+string userInput1 = Console.ReadLine ()?? "";
+int N = int.Parse (userInput1);
+int count = 1;
+Console.Write ($"{N} -> ");
+while (N >= count)
+{
+    Console.Write ($"{Math.Pow(count,3)}, ");
+    count++;
+}
