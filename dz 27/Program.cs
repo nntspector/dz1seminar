7d47945c31b235fa760061b958e87d9de25a43c0
Pavel <nntspector@icloud.com>
2023-01-14 13:51:41 +0300

    zadacha 27 reshena

---
+++ dz 27/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Clear();
+int num = GetNumberFromUser("Введите число: ", "Ошибка Ввода!");
+
+int sumNumbers = SumNumbers(num);
+
+Console.WriteLine ($"{num} -> {sumNumbers}");
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+int SumNumbers(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return result;
+}
