63db62409f8c0f3f3b73769f3f7b6b749791ff71
Pavel <nntspector@icloud.com>
2023-01-15 19:43:00 +0300

    29 +

---
+++ dz 29/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+
+
+// Console.Clear();
+// int num = GetNumberFromUser("Введите первое число массива: ", "Ошибка Ввода!");
+// int num1 = GetNumberFromUser("Введите второе число массива: ", "Ошибка Ввода!");
+// int num2 = GetNumberFromUser("Введите третье число массива: ", "Ошибка Ввода!");
+// int num3 = GetNumberFromUser("Введите четвертое число массива: ", "Ошибка Ввода!");
+// int num4 = GetNumberFromUser("Введите пятое число массива: ", "Ошибка Ввода!");
+// int num5 = GetNumberFromUser("Введите шестое число массива: ", "Ошибка Ввода!");
+// int num6 = GetNumberFromUser("Введите седьмое число массива: ", "Ошибка Ввода!");
+// int num7 = GetNumberFromUser("Введите восьмое число массива: ", "Ошибка Ввода!");
+
+// Console.WriteLine ($"{num}, {num1}, {num2}, {num3}, {num4}, {num5}, {num6}, {num7} -> [{num}, {num1}, {num2}, {num3}, {num4}, {num5}, {num6}, {num7}]");
+
+// int GetNumberFromUser(string message, string errorMessage)
+// {
+//     while (true)
+//     {
+//         Console.Write(message);
+//         bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+//         if (isCorrect)
+//             return userNumber;
+//         Console.WriteLine(errorMessage);
+//     }
+// }
+
+
+
+// arrrandom();
+
+// void arrrandom()
+// {
+//     Console.Write("[");
+//     for (int i = 8; i > 0; i--)
+//     {
+//         int num = new Random().Next();
+//         Console.Write($"{num}");
+//         if (i > 1)
+//         {
+//             Console.Write(",");
+//         }
+//     }
+//     Console.Write("]");
+// }
+
+
+
