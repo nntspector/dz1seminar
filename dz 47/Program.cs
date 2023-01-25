16eb4c4075e567ceed7a2eeb4ced73d99a1c955b
Pavel <nntspector@icloud.com>
2023-01-25 21:28:05 +0300

    zadacha 47 reshena

---
+++ dz 47/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+Random rnd = new Random ();
+Console.Write ("Введите количество строк (m): ");
+int m = Convert.ToInt32 (Console.ReadLine ());
+
+Console.Write ("Введите количество столбцов (n): ");
+int n = Convert.ToInt32 (Console.ReadLine ());
+
+double [,] arr = new double[m,n];
+// диапазон массива
+int min = 1;
+int max = 10;
+////////////////////////
+
+for (int i = 0; i<m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        arr[i,j] = min +(max-min)* rnd.NextDouble ();
+    }
+}
+Console.WriteLine ($"{m} = m, {n} = n.");
+PrintArray (arr);
+
+void PrintArray(double [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
