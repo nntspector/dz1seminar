58c616bac58cb36ee324b19e342798438ae3b99f
Pavel <nntspector@icloud.com>
2023-02-05 11:43:55 +0300

    zadacha 66 reshena

---
+++ dz 66/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int m = GetNumberFromUser("Введите число M:", "Ошибка ввода!");
+int n = GetNumberFromUser("Введите число N:", "Ошибка ввода!");
+Console.WriteLine ($"M = {m}, N = {n} -> {FindSum (m,n)}");
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+ int FindSum(int m, int n)
+{
+    if (m > n)
+    {
+        return 0;
+    }
+    else
+    {
+        return m + FindSum(m + 1, n);
+    }
+
+}
